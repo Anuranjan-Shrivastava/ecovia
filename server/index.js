0d897aad17f52cbebff4cb91e47963a9dc7e5340
Anuranjan-Shrivastava <s.anuranjan.shrivastava@gmail.com>
2023-12-07 02:00:17 +0530

    Modified cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const passport_jwt = require('./config/passport-jwt') ; ;
 
 
 //middleware 
-app.use(cors()) ;
+app.use(cors({credentials: true, origin: 'https://ecovia-frontend.vercel.app'}));
 app.use(express.json()) ;
 app.use(express.urlencoded({
     extended : false  , 
